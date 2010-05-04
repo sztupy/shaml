f9058457492f53d871cd1788a707a5bf7d7b148e
SztupY <mail@sztupy.hu>
2010-05-04 17:38:32 +0200

    Added IdHelperFor command

---
+++ shaml/Shaml.Web/HtmlHelpers/IdHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Reflection;
+using System.ComponentModel;
+using Shaml.Core;
+using System.Linq.Expressions;
+
+namespace Shaml.Web.HtmlHelpers
+{
+    static public class IdHelper
+    {
+        static public MvcHtmlString IdHelperFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return MvcHtmlString.Create(htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(ExpressionHelper.GetExpressionText(expression)));
+        }
+    }
+}
